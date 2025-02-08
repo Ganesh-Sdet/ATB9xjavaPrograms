58a73c2cc1543015d6c44ec110d99fcfb981da33
Ganesh <ganeshsuryawanshi1306@gmail.com>
2025-02-09 02:06:12 +0530

    My Files

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,11 @@
+package Oct.ex_03102024;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.sql.SQLOutput;
+
+public class Lab002 {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+
+}
